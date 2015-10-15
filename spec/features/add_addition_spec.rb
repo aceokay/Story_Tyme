b4aa270857d24aa461458a6a3f167566a5b93eac
Alexa Zeazas & Austin Kincaid <student@epicodus.com>
2015-10-15 10:07:30 -0700

    Add integration spec for adding new additions

---
+++ spec/features/add_addition_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "the add an addition process" do
+  it "adds an addition to a story" do
+    visit stories_path
+    click_on "Add a New Story"
+    fill_in 'Title', :with => "Deez Nuts"
+    fill_in 'Starter sentence for your story', :with => "There was a man with nuts."
+    fill_in 'Your Name', :with => "Dee"
+    click_on 'Submit'
+    expect(page).to have_content "Deez Nuts"
+    click_on "Deez Nuts"
+    click_on "Add to this story!"
+    fill_in 'Make Your Addition (only one sentence!)', :with => "So spoopy!"
+    fill_in 'Your Name', :with => "Steve"
+    click_on 'Submit'
+  end
+
+  it "gives error when no name is entered" do
+    visit stories_path
+    click_on "Add a New Story"
+    fill_in 'Title', :with => "Deez Nuts"
+    fill_in 'Starter sentence for your story', :with => "There was a man with nuts."
+    fill_in 'Your Name', :with => "Dee"
+    click_on 'Submit'
+    expect(page).to have_content "Deez Nuts"
+    click_on "Deez Nuts"
+    click_on "Add to this story!"
+    click_on "Submit"
+    expect(page).to have_content 'errors'
+  end
+end

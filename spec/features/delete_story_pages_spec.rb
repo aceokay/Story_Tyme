5e114c414f7fbe267abe44ac071a566821612744
Alexa Zeazas & Austin Kincaid <student@epicodus.com>
2015-10-15 10:55:46 -0700

    Add delete path integration specs

---
+++ spec/features/delete_story_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "the delete a story process" do
+  it "deletes a story" do
+    visit stories_path
+    click_on "Add a New Story"
+    fill_in 'Title', :with => "Deez Nuts"
+    fill_in 'Starter sentence for your story', :with => "There was a man with nuts."
+    fill_in 'Your Name', :with => "Dee"
+    click_on 'Submit'
+    click_on 'Deez Nuts'
+    click_on 'Delete'
+    expect(page).to_not have_content "Deez Nuts"
+    expect(page).to have_content "Active Stories"
+  end
+end

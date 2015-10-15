08f7a9c3f5756af26e2253194774d1a517a0bdae
Alexa Zeazas & Austin Kincaid <student@epicodus.com>
2015-10-15 11:06:34 -0700

    Add integration spec for addition delete path

--- spec/features/delete_addition_pages_spec.rb
+++ spec/features/delete_addition_pages_spec.rb
@@ -8,26 +8,12 @@ describe "the add an addition process" do
     fill_in 'Starter sentence for your story', :with => "There was a man with nuts."
     fill_in 'Your Name', :with => "Dee"
     click_on 'Submit'
-    expect(page).to have_content "Deez Nuts"
     click_on "Deez Nuts"
     click_on "Add to this story!"
     fill_in 'Make Your Addition (only one sentence!)', :with => "So spoopy!"
     fill_in 'Your Name', :with => "Steve"
     click_on 'Submit'
-    expect(page).to have_content "So spoopy!"
-  end
-
-  it "gives error when no name is entered" do
-    visit stories_path
-    click_on "Add a New Story"
-    fill_in 'Title', :with => "Deez Nuts"
-    fill_in 'Starter sentence for your story', :with => "There was a man with nuts."
-    fill_in 'Your Name', :with => "Dee"
-    click_on 'Submit'
-    expect(page).to have_content "Deez Nuts"
-    click_on "Deez Nuts"
-    click_on "Add to this story!"
-    click_on "Submit"
-    expect(page).to have_content 'errors'
+    click_on 'Delete Contribution'
+    expect(page).to_not have_content "So spoopy!"
   end
 end
